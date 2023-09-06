08711058cdb86dce2a47d82598f222d35250189e
Burak Mat <burakk.mat@gmail.com>
2023-09-06 11:34:44 +0300

    add modal-home page sync for group addition/deletion

--- frontend/src/components/forms/GroupCreation.tsx
+++ frontend/src/components/forms/GroupCreation.tsx
@@ -1,125 +1,130 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { typeUser } from "../../types"
-import { createGroup, getGroups } from "../../api/room";
+import { createGroup, getGroups, getUsersRooms } from "../../api/room";
+import { UserContext } from "../../context/context";
 
 export default function GroupCreation(
-	{
-		friends,
+  {
+    friends,
     parentRef,
     setGroups,
     handleListData,
-    groupsButtons
-	}: {
-		friends: typeUser[]
+    groupsButtons,
+    setAllRooms
+  }: {
+    friends: typeUser[]
     parentRef: any
     setGroups: Function
     handleListData: Function
     groupsButtons: any
-	}
-	) {
-    const [checkboxes, setCheckboxes] = useState([])
-    const [groupName, setGroupName] = useState("")
-    const [password, setPassword] = useState("")
+    setAllRooms: Function
+  }
+) {
+  const [checkboxes, setCheckboxes] = useState([])
+  const [groupName, setGroupName] = useState("")
+  const [password, setPassword] = useState("")
+  const { user } = useContext(UserContext)
 
-    const handleCheckboxChange = (value: string, checked: boolean) => {
-      if (checked) {
-          setCheckboxes([...checkboxes, value]);
-      } else {
-          setCheckboxes(checkboxes.filter((item) => item !== value));
-      }
+  const handleCheckboxChange = (value: string, checked: boolean) => {
+    if (checked) {
+      setCheckboxes([...checkboxes, value]);
+    } else {
+      setCheckboxes(checkboxes.filter((item) => item !== value));
+    }
   };
 
   // useEffect(() => {
   //   console.log("friends", friends)
   // }, [friends])
-// useEffect(() => {
-//   console.log("current checkbox", checkboxes)
-// }, [checkboxes])
+  // useEffect(() => {
+  //   console.log("current checkbox", checkboxes)
+  // }, [checkboxes])
 
-const hidePassword = () => {
-  let node = document.querySelector("#password")
-  node.setAttribute("type", "password")
-  document.querySelector("#password_icon").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/2767/2767146.png")
-}
+  const hidePassword = () => {
+    let node = document.querySelector("#password")
+    node.setAttribute("type", "password")
+    document.querySelector("#password_icon").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/2767/2767146.png")
+  }
 
-const showPassword = () => {
-  let node = document.querySelector("#password")
-  node.setAttribute("type", "text")
-  document.querySelector("#password_icon").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/159/159604.png")
-}
-	return (
-			<form onSubmit={async (e) => {
-        e.preventDefault()
-        let payload = password === "" ? {
-          name: groupName,
-          users: checkboxes
-        } : {
-          name: groupName,
-          users: checkboxes,
-          password: password
-        }
-        await createGroup(payload)
-        parentRef.current.close()
-        document.querySelectorAll(".group_creation_input").forEach((item: any) => {
-          item.checked = false
-        })
-        setGroupName("")
-        setCheckboxes([])
-        setPassword("")
-        handleListData(await getGroups(setGroups), groupsButtons)
-        document.querySelector("#password").setAttribute("type", "password")
-      }}>
-				<div>
-			  	<label htmlFor="name">Group Name:</label>
-			  	<input 
-            type="text" 
-            id="name" 
-            value={groupName}
-            onChange={(e) => {setGroupName(e.target.value)}}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password" 
-            placeholder="optional"
-            onChange={(e) => {setPassword(e.target.value)}}
-          />
-          <img 
-            id="password_icon"
-            src="https://cdn-icons-png.flaticon.com/512/2767/2767146.png" width={20} alt="show password" 
-            onClick={() => {
-              if (document.querySelector("#password").getAttribute("type") === "password") {
-                showPassword()
-              } else {
-                hidePassword()
-              }
-            }}
-          />
-        </div>
-				<div>
-          Group Members:
-					{friends.map((friend: typeUser) => (
-						<div key={friend.id}>
-							<input 
+  const showPassword = () => {
+    let node = document.querySelector("#password")
+    node.setAttribute("type", "text")
+    document.querySelector("#password_icon").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/159/159604.png")
+  }
+  return (
+    <form onSubmit={async (e) => {
+      e.preventDefault()
+      let payload = password === "" ? {
+        name: groupName,
+        users: checkboxes
+      } : {
+        name: groupName,
+        users: checkboxes,
+        password: password
+      }
+      await createGroup(payload)
+      parentRef.current.close()
+      document.querySelectorAll(".group_creation_input").forEach((item: any) => {
+        item.checked = false
+      })
+      setGroupName("")
+      setCheckboxes([])
+      setPassword("")
+      handleListData(await getGroups(setGroups), groupsButtons)
+      await getUsersRooms(setAllRooms, user)
+      document.querySelector("#password").setAttribute("type", "password")
+    }}>
+      <div>
+        <label htmlFor="name">Group Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={groupName}
+          onChange={(e) => { setGroupName(e.target.value) }}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="optional"
+          onChange={(e) => { setPassword(e.target.value) }}
+        />
+        <img
+          id="password_icon"
+          src="https://cdn-icons-png.flaticon.com/512/2767/2767146.png" width={20} alt="show password"
+          onClick={() => {
+            if (document.querySelector("#password").getAttribute("type") === "password") {
+              showPassword()
+            } else {
+              hidePassword()
+            }
+          }}
+        />
+      </div>
+      <div>
+        Group Members:
+        {friends.map((friend: typeUser) => (
+          <div key={friend.id}>
+            <input
               className="group_creation_input"
-              type="checkbox" 
-              name={friend.username} 
-              id={friend.username} 
+              type="checkbox"
+              name={friend.username}
+              id={friend.username}
               value={friend.username}
               onChange={
                 (e) => handleCheckboxChange(e.target.value, e.target.checked)
-                }
-              />
-							<label htmlFor={friend.username}>{friend.username}</label>
-						</div>
-					))}
+              }
+            />
+            <label htmlFor={friend.username}>{friend.username}</label>
+          </div>
+        ))}
 
-				</div>
-        <button disabled={groupName === "" ? true : false}>Create</button>
-			</form>
+      </div>
+      <button disabled={groupName === "" ? true : false}>Create</button>
+    </form>
 
-	)
+  )
 }
