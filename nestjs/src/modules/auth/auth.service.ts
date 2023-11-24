feebdf5a0d2df9d86ce7f9a74dcc3a512aeb2909
osman yalçın <osmanyalcin989@gmail.com>
2023-11-24 15:16:28 +0300

    unnecessary deletes

--- nestjs/src/modules/auth/auth.service.ts
+++ nestjs/src/modules/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/typeorm/User';
-import { twoFactorDto } from 'src/types/2fa.dto';
 import * as speakeasy from 'speakeasy';
 @Injectable()
 export class AuthService {
