5550d0994cf6eecb049ffd88a691865a29f8c306
Burak Mat <burakk.mat@gmail.com>
2023-12-02 15:03:18 +0300

    notification list can be closed by notification bell

--- frontend/src/components/Notification/Notification.tsx
+++ frontend/src/components/Notification/Notification.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import image from "../../public/notification.png"
 import { UserContext } from "../../context/UserContext"
 import './Notification.scss'
@@ -6,9 +6,17 @@ import { ContextMenuContentType } from "../../types"
 import { ContextMenuContext } from "../../context/ContextMenuContext"
 
 export default function Notification() {
-
   const { notifications } = useContext(UserContext)
-  const { openContextMenu } = useContext(ContextMenuContext)
+  const { openContextMenu, closeContextMenu, nonModalActive } = useContext(ContextMenuContext)
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    if (open && !nonModalActive) {
+      setOpen(false)
+    }
+  
+  }, [nonModalActive])
+  
 
   return (
     <div className="notification-button">
@@ -17,22 +25,19 @@ export default function Notification() {
           onClick={(event) => {
             event.stopPropagation()
             const node = event.target as HTMLElement
-            // const position: NonModalPosition = {}
-            // position["left"] = node.getBoundingClientRect().left
-            // position["top"] = node.getBoundingClientRect().bottom
-            
-            // setPosition(position)
-            // setNonModalActive(!nonModalActive)
-            openContextMenu(node.getBoundingClientRect().left, 
-                            node.getBoundingClientRect().bottom,
-                            ContextMenuContentType.NOTIFICATION, null)
+            if (open) {
+              setOpen(false)
+              closeContextMenu()
+            } else {
+              setOpen(true)
+              openContextMenu(node.getBoundingClientRect().left, 
+              node.getBoundingClientRect().bottom,
+              ContextMenuContentType.NOTIFICATION, null)
+            }
           }}
         />
         <div className={"new-notification" + (notifications?.length ? "" : " hidden")}></div>
       </div>
-      {/* <NonModal isActive={[modal, setModal]} dialogPosition={dialogPosition.current}>
-        <NotificationList />
-      </NonModal> */}
     </div>
   )
 }
